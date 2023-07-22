ac562790af6a692bfe035833f27d2a6c3ddc0365
amkkr <55781271+amkkr@users.noreply.github.com>
2023-07-22 12:13:15 +0900

    add: key.tsx

---
+++ routes/example/[key].tsx
@@ -0,0 +1,18 @@
+import {Handlers} from "$fresh/server.ts";
+
+export const handler: Handlers = {
+    async GET(_req, ctx) {
+        const resp =await ctx.render();
+        resp.headers.set("custom_header", "Hello");
+        return resp;
+    },
+};
+
+export default function Example() {
+    return (
+    <main>
+        <h1>Example</h1>
+        <p>Example Page</p>
+    </main>
+    )
+}
