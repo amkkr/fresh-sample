714bd0acacde2ad655d9ac0c89b9baf8500d2e1f
amkkr <55781271+amkkr@users.noreply.github.com>
2023-07-22 12:28:56 +0900

    fix: fmt

--- routes/example/[key].tsx
+++ routes/example/[key].tsx
@@ -1,18 +1,18 @@
-import {Handlers} from "$fresh/server.ts";
+import { Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers = {
-    async GET(_req, ctx) {
-        const resp =await ctx.render();
-        resp.headers.set("custom_header", "Hello");
-        return resp;
-    },
+  async GET(_req, ctx) {
+    const resp = await ctx.render();
+    resp.headers.set("custom_header", "Hello");
+    return resp;
+  },
 };
 
 export default function Example() {
-    return (
+  return (
     <main>
-        <h1>Example</h1>
-        <p>Example Page</p>
+      <h1>Example</h1>
+      <p>Example Page</p>
     </main>
-    )
+  );
 }
